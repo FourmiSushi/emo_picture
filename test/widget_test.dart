6155e8aa778a89828a23102d9f70f683f90f910e
FourmiSushi <yogurtq@neko.kawaii>
2018-11-28 14:25:38 +0900

    pictures.dartを追加、widget_test.dartがエラーでうるさいので修正

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:emo_picture/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(EmoPictureApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

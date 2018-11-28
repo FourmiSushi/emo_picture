d8f11e0905fc30577293c35a3b61073c61c89d7f
FourmiSushi <yogurtq@neko.kawaii>
2018-11-28 23:47:55 +0900

    pageviewの中身をImageに変更したよ

--- lib/pictures.dart
+++ lib/pictures.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 
 class PicturesState extends State<Pictures> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(top: 64.0, bottom: 64.0),
       child: PageView(
         controller: PageController(viewportFraction: 0.75),
         children: <Widget>[
-          Text('ここに画像が入るよ1'),
-          Text('ここに画像が入るよ2'),
-          Text('ここに画像が入るよ3'),
+          Image(
+            image: AssetImage('assets/images/textpic1.png'),
+          ),
+          Image(
+            image: AssetImage('assets/images/textpic2.png'),
+          ),
+          Image(
+            image: AssetImage('assets/images/textpic3.png'),
+          ),
         ],
       ),
     );

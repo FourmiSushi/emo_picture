dc21ab2e61269b4964875dca721779e45930e824
FourmiSushi <yogurtq@neko.kawaii>
2018-11-29 12:39:27 +0900

    いい感じに見えるようにしてふぁぼぼたんを追加（押しても何もない）したよ

--- lib/pictures.dart
+++ lib/pictures.dart
@@ -7,22 +7,43 @@ class PicturesState extends State<Pictures> {
       padding: EdgeInsets.only(top: 64.0, bottom: 64.0),
       child: PageView(
         controller: PageController(viewportFraction: 0.85),
-        children: <Widget>[
-          /* Image(
-            image: AssetImage('assets/images/textpic1.png'),
-          ),
-          Image(
-            image: AssetImage('assets/images/textpic2.png'),
-          ),
-          Image(
-            image: AssetImage('assets/images/textpic3.png'),
-          ), */
-          Image.network('https://picsum.photos/800/1350/?random'),
-          Image.network('https://picsum.photos/800/1350/?random&'),
-          Image.network('https://picsum.photos/800/1350/?random&&'),
-        ],
+        children: List.generate(5, (i) => pageImage(i)),
       ),
     );
+  }
+
+  Padding pageImage(int page) {
+    String additionaltext = '&' * page;
+    bool favorited = false;
+    return Padding(
+        padding: EdgeInsets.all(16),
+        child: Container(
+          decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(16.0),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black45,
+                  blurRadius: 4.0,
+                  spreadRadius: 2.0,
+                  offset: Offset(0.0, 4.0),
+                ),
+              ],
+              image: DecorationImage(
+                  image: NetworkImage(
+                      'https://picsum.photos/800/1400/?random$additionaltext'),
+                  fit: BoxFit.fitWidth)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(favorited ? Icons.favorite : Icons.favorite_border, color: favorited ? Colors.red : Colors.white54),
+                iconSize: 48,
+                padding: EdgeInsets.all(16),
+              )
+            ],
+          ),
+        ));
   }
 }
 
@@ -30,4 +51,3 @@ class Pictures extends StatefulWidget {
   @override
   PicturesState createState() => PicturesState();
 }
-// memo なんかに入れてBoxFitする　bookreaderを参考にすればいける
